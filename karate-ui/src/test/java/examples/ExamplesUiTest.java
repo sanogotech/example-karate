f6131e757550874d98cd867366b1edaab54d70cb
SOULEYSANOGO <SOULEYSANOGO@CIEW15854>
2021-07-14 16:46:25 +0000

    Update

--- karate-ui/src/test/java/examples/ExamplesUiTest.java
+++ karate-ui/src/test/java/examples/ExamplesUiTest.java
@@ -4,13 +4,14 @@ import com.intuit.karate.junit5.Karate;
 
 class ExamplesUiTest {
 
-    // @Karate.Test
-    // Karate testAll() {
-    // return Karate.run().relativeTo(getClass());
-    // }
-
     @Karate.Test
-    Karate testExcludesIgnore() {
-        return Karate.run().tags("~@ignore").relativeTo(getClass());
+    Karate testAll() {
+		return Karate.run().relativeTo(getClass());
     }
+
+	
+    // @Karate.Test
+    // Karate testExcludesIgnore() {
+        // return Karate.run().tags("~@ignore").relativeTo(getClass());
+    // }
 }
